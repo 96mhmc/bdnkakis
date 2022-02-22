f5ba677528ce3c83456642b0c4e01246dc0e0e87
96mhmc <73679702+96mhmc@users.noreply.github.com>
2022-02-22 17:10:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ with form:
     comment = st.text_area("Remarks if any:")
     submitted = st.form_submit_button(label="Submit")
 
-    
+"""    
 if submitted:
     timestmp=datetime.now(tz=None)
     add_row_to_gsheet(
@@ -103,3 +103,75 @@ expander = st.expander("See all records")
 with expander:
     st.write(f"Open original [Google Sheet]({GSHEET_URL})")
     st.dataframe(get_data(gsheet_connector))
+"""
+if submitted:
+#    st.write(datetime.now(tz=None))
+    timestmp=datetime.now(tz=None)
+    add_row_to_gsheet(
+        gsheet_connector, [[player, str(date), venue, shuttles, hours, courts, comment,str(timestmp)]]
+    )
+    st.success("Thanks! Your participation has been recorded.")
+    st.balloons()
+
+expander = st.expander("See Accounts Status")
+with expander:
+#    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+#    st.dataframe(get_data(gsheet_connector))
+    df=get_data(gsheet_connector)
+    #st.write(df)
+
+##START HERE
+    # Fixed Data
+    ShuttleCost=2.75
+    dict_attendance = {"Yes and attended session." : 1, "No." : 1,"Yes but did not attend session.":0}
+    dict_courts={"Yes and attended session." : 1, "No." : 0,"Yes but did not attend session.":1}
+    dict_recon={"Yes and attended session." : 0, "No." : 0,"Yes but did not attend session.":1}
+    dict_venue={"Clementi Sports Hall":7.4,"Mt Faber SAFRA":5.2,"Chinese Swimming Club":9}
+
+    # Derive dataframe
+    df.insert(8,'Attendance', df['PaymentnAttendance'] )
+    df['Attendance']=df['Attendance'].replace(to_replace=dict_attendance)
+    df.insert(9,'Courts', df['PaymentnAttendance'] )
+    df['Courts']=df['Courts'].replace(to_replace=dict_courts)
+    df.insert(10,'Recon1', df['PaymentnAttendance'] )
+    df['Recon1']=df['Recon1'].replace(to_replace=dict_recon)
+    df.insert(11,'VenueCost', df['Venue'] )
+    df['VenueCost']=df['VenueCost'].replace(to_replace=dict_venue)
+    df['ShuttleExpense']=pd.to_numeric(df.Shuttles)*ShuttleCost  #change df.Shuttles to float
+    df['CourtExpense']=df.Courts*pd.to_numeric(df.Hours)*df.VenueCost
+    df['Recon1']=df['Recon1']*df['CourtExpense']
+
+    # Pivot Table
+    df_Attendance=df.pivot_table(index='Date',columns='Player',values='Attendance',fill_value=0)
+    df_Shuttle=df.pivot_table(index='Date',columns='Player',values='ShuttleExpense',aggfunc=np.sum,fill_value=0)
+    df_Court=df.pivot_table(index='Date',columns='Player',values='CourtExpense',fill_value=0)
+    df_Recon1=df.pivot_table(index='Date',columns='Player',values='Recon1',fill_value=0)#*df1.pivot_table(index='Date',columns='PlayerID',values='Hours',fill_value=0)
+    
+    #st.write(df.Player)
+#    st.write(df_Attendance)
+    
+    df_Cost=df_Court+df_Shuttle
+    df_TotalCost=df_Cost.sum(axis=1)
+    df_AverageCost=df_TotalCost/df_Attendance.sum(axis=1)
+#    st.write(df_AverageCost)
+
+    # Process Pivot Table
+    df_Recon2=df_Cost.sub(df_AverageCost, axis='rows')*df_Attendance
+    df_ReconNew=df_Recon1+df_Recon2
+    df_ReconNew.sum(axis=1)
+    df_ReconNew.loc['Total']=df_ReconNew.sum(axis=0)
+    df_ReconNew['CheckSum']=df_ReconNew.sum(axis=1)
+    st.write(df_ReconNew)
+#    st.write(df_ReconNew.sum(axis=0))
+ #   st.write(df_ReconNew.sum(axis=0).to_frame().T)    
+##END HERE
+
+expander2= st.expander("See Raw Data")
+with expander2:
+#    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+    st.dataframe(get_data(gsheet_connector))
+
+#st.container()
+#with st.container():
+#    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
+#    st.dataframe(get_data(gsheet_connector))
