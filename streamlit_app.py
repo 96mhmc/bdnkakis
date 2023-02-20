04d8ba6f586a7125822980d67cdf4c04ef4bed8c
iloveroro-hi@yahoo.com <cmianghw@gmail.com>
2023-02-20 21:04:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,13 +71,8 @@ with form:
 #    date = cols[1].date_input("Date of session played or court booked:")
     session_name = cols[0].selectbox(
         "Session:", [
-<<<<<<< Updated upstream
-        "3 Mar 2022, Chinese Swimming Club, 1 hour"
-        "23 Feb 2022, Clementi Sports Hall, 1 hour",
-=======
         "3 Mar 2022, Chinese Swimming Club, 1 hour",    
         "23 Mar 2022, Clementi Sports Hall, 1 hour",
->>>>>>> Stashed changes
         "17 Feb 2022, Chinese Swimming Club, 1 hour", 
         "7 Feb 2022, Clementi Sports Hall, 1 hour"]
     ) #, index=2
@@ -107,71 +102,6 @@ if submitted:
 
 expander= st.expander("Raw Data from 7 Feb 2022")
 with expander:
-<<<<<<< Updated upstream
-    #st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-    st.dataframe(get_data(gsheet_connector))
-
-
-expander2 = st.expander("See Accounts Status")
-with expander2:
- #    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
- #    st.dataframe(get_data(gsheet_connector))
-    df=get_data(gsheet_connector)
-
-    # Fixed Data
-    shuttle_cost=2.75
-    dict_attendance = {"Yes" : 1, "No" : 0}
-    dict_venue_cost={"Clementi Sports Hall":7.4,"Mt Faber SAFRA":5.2,"Kim Seng CC":5,"Chinese Swimming Club":9}
-    dict_court_payment = {"Yes" : 1, "No" : 0}
-    csc_entrancefee=3
-#    dict_case2_venue={"Clementi Sports Hall":0,"Mt Faber SAFRA":0,"Kim Seng CC":0,"Chinese Swimming Club":1}
-#    dict_case2_player={"Mike":0,"Hugo Boss":0, "Wing Gor":0,"Super Stan":0,"Soon":1}
-
-    # Derive dataframe
-    df[['date', 'venue', 'hour']] = df['session_name'].str.split(',', expand=True)
-    df['date']=pd.to_datetime(df['date'])#,format='%d %b %y')
-    df['hour']=pd.to_numeric(df['hour'].str.rstrip('hour'))
-    #df.venue=df.venue.str.replace(' ','')
-    df['court_payment']=pd.to_numeric(df['court_payment'].replace(dict_court_payment,regex=True))
-    df['attendance']=df['attendance'].replace(dict_attendance, regex=True)  
-    df['shuttle_expense']=pd.to_numeric(df['shuttles_contributed'])*shuttle_cost
-    df['court_expense']=df['venue'].replace(dict_venue_cost,regex=True)*df['hour']*df['court_payment']
-    df['case1']=df['court_payment']-df['attendance']
-    df['case1']=df['case1'].loc[df['case1']>0]
-    df['case1']=df['case1'].fillna(0)
-#    df['case21']=df.venue.apply(lambda venue_var: 1 if venue_var==" Chinese Swimming Club" else 0)
-#    df['case22']=df.player_name.apply(lambda player_var: 1 if player_var=="Soon" else 0)
-    df["case21"]=0
-    df.loc[(df.venue==" Chinese Swimming Club") & (df.player_name=="Soon"), "case21"] = 1
-    df['case22']=0
-    df.loc[(df.venue==" Chinese Swimming Club") & (df.player_name!="Soon"), "case22"] = 1
-   
-
-    # Pivot Table
-    df_attendance=df.pivot_table(index='date',columns='player_name',values='attendance',fill_value=0)
-    df_shuttle=df.pivot_table(index='date',columns='player_name',values='shuttle_expense',aggfunc=np.sum,fill_value=0)
-    df_court=df.pivot_table(index='date',columns='player_name',values='court_expense',fill_value=0)
-    df_case1=df.pivot_table(index='date',columns='player_name', values='case1',fill_value=0)*df_court
-    df_case21=df.pivot_table(index='date',columns='player_name', values='case21',fill_value=0)
-    df_case22=df.pivot_table(index='date',columns='player_name', values='case22',fill_value=0)*csc_entrancefee*(-1)
-    df_expense=df_court+df_shuttle
-    df_totalexpense=df_expense.sum(axis=1)
-    df_averageexpense=df_totalexpense/df_attendance.sum(axis=1)
-    df_csc_entrance=df_case22.sum(axis=1)
-    df_case21["Soon"]=df_case21["Soon"]*df_csc_entrance*(-1)
-
-    # Process Pivot Tables
-    df_recon=df_expense.sub(df_averageexpense, axis='rows')*df_attendance
-    df_recon=df_recon+df_case1+df_case21+df_case22
-    #df_recon.loc["total"]=df_recon[1:-1].sum(axis=1)
-    df_recon['CheckSum']=df_recon.sum(axis=1)
-    #st.dataframe(df_recon.dtype)
-    #st.write('Recon')
-    #st.write(df_recon)
-    st.dataframe(df_recon.style.format("{:.2f}"))
-    st.write("Total")
-    st.dataframe(df_recon.sum(axis=0).to_frame().T)
-=======
     st.write(f"Open original [data]({GSHEET_URL})")
     st.dataframe(get_data(gsheet_connector)[["player_name","session_name","attendance","shuttles_contributed","court_payment","comments"]])
 
@@ -248,7 +178,6 @@ with expander3:
 #     st.write()
 #     st.write("Total")
 # #    st.dataframe(df_recon.sum(axis=0).to_frame().T)
->>>>>>> Stashed changes
     
 
     
